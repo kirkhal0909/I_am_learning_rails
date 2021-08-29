dc8fd6fff170595ee393a570907b9fdf16caebd7
kirkhal0909 <kirkhal0909@gmail.com>
2021-08-29 16:04:56 +0300

    created object - Microposts. Added relation to User and validates

--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
+    validates :name, format: {with: /\A[\w]{5,40}\z/,
+                              message: "; Accepted characters [a-z, A-Z, _]; length from 5 to 40"}
+    validates :email, format: { with: /\A[-\w]{2,100}@[-\w.]{2,100}\z/,
+                                message: '; Please, enter valid email (user@domain.com)' }
 end
