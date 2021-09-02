f7261fe39e74adf905f036703cef48b30dcdfa78
kirkhal0909 <kirkhal0909@gmail.com>
2021-09-02 09:38:38 +0300

    Added pages with bootstrap

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'signup' => 'users#new'
 
   root 'static_pages#home'
 
